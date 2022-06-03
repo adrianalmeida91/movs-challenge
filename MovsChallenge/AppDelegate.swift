c0bd4fbe677a109364cdad8d766f1987167de6be
Adrian Almeida <adrian.almeida@picpay.com>
2022-06-03 17:00:45 -0300

    feature: add initial project

---
+++ MovsChallenge/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MovsChallenge
+//
+//  Created by adrian.almeida on 03/06/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
---
+++ MovsChallenge/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MovsChallenge
+//
+//  Created by adrian.almeida on 03/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
